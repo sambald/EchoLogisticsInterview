dcae4b7ffb9d8680cad53fb5ffd60cc4fb1a9f98
Sam Bald <sambald156@gmail.com>
2022-03-10 16:40:20 -0600

    Added create recipe

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This is my interview demo. There's a backend, written in C# that uses Entity Fra
 There's a basic frontend (static html/js served by nginx) with three pages: a login page, a page to view all recipes, and a page to view an individual recipe.
 
 ## API 
-Here are some example routes in the backend API with screenshots. 
+Here are some example routes in the backend API with screenshot. 
 
 `GET: /api/recipes` This route returns a JSON list of all the recipes in the postgres database. 
 
@@ -16,6 +16,8 @@ Here are some example routes in the backend API with screenshots.
 
 `POST /api/validateLogin` This route expects a `{ name: "...", password: "..." }` to be posted and will return an OK response if the user exists in the database with a matching password. NOTE: I stored passwords in plaintext here, but in reality you wouldn't want to store any sensitive info in plaintext... 
 
+![API Screenshot](docs/api_screenshot.png)
+
 ## Database
 I'm using a postgres database (`DB=interview` `user=postgres`) to store data on the recipes, users, and ingredients. I didn't write a schema manually, instead I used Entity Framework Core's "code first" models to generate a schema for the DB automatically. (I have a debug `GET /api/resetDB` route that allows me to delete/re-create the DB.)
 
@@ -23,10 +25,13 @@ I'm using a postgres database (`DB=interview` `user=postgres`) to store data on 
 I have a handful of pages for the frontend. I'm by no means a frontend expert, so I googled around for a basic library to use and settled on Material Design Lite (MDL). I use MDL for layout/cards/lists and use vanilla JavaScript to dynamically compose some things based on responses from the backend API.
 
 ### Page: `/login.html`
-![Frontend Screenshot](docs/frontend_screenshot.png)
+![Login Screenshot](docs/login_screenshot.png)
 
 ### Page: `/recipes.html?userId=<Id>`
 ![Recipes Screenshot](docs/recipes_screenshot.png)
 
 ### Page: `/recipe.html?recipeId`
 ![Recipe Screenshot](docs/recipe_screenshot.png)
+
+### Page: `/createRecipe.html?recipeId`
+![Create Recipe Screenshot](docs/createRecipe_screenshot.png)
--- frontend/static/script.js
+++ frontend/static/script.js
@@ -1,4 +1,4 @@
-//Verifies a valid user
+//Logs in an existing user
 let onLoginClick = () => {
 
     //Clears error message
@@ -15,11 +15,11 @@ let onLoginClick = () => {
             password: document.getElementById("password").value
         })
     }).then(res => {
-        //If invalid password, display error message
+        //If invalid username or password, display error message
         if (res.status != 200) {
             document.getElementById("badPassword").style.display = "block";
         }
-        //If valid password, log in user and display their recipes
+        //If valid user and password, log in user and display their recipes
         else {
             document.getElementById("badPassword").style.display = "none";
             res.json().then(json => {
@@ -50,10 +50,10 @@ let onRegisterClick = () => {
         if (res.status != 200) {
             document.getElementById("badRegistration").style.display = "block";
             res.json().then(json => {
-                if (json.message != null){
+                if (json.message != null) {
                     document.getElementById("badRegistration").innerText = json.message;
                 }
-                else{
+                else {
                     document.getElementById("badRegistration").innerText = "Incomplete username or password";
                 }
             });
@@ -68,7 +68,7 @@ let onRegisterClick = () => {
     });
 };
 
-//If user presses enter key after typing in their password, activates on click event
+//If user presses enter key after typing in their password, activates log in button
 let onPasswordEnter = (event) => {
     if (event.key === 'Enter') {
         onLoginClick();
@@ -80,6 +80,8 @@ let buildRecipes = () => {
     const urlSearchParams = new URLSearchParams(window.location.search);
     const params = Object.fromEntries(urlSearchParams.entries());
 
+    document.getElementById('create-recipe-button').href = "createRecipe.html?userId=" + params.userId;
+
     fetch("/api/users/" + params.userId + "/recipes", {
         method: "GET"
     }).then(
@@ -87,20 +89,20 @@ let buildRecipes = () => {
 
             json.forEach(recipe => {
 
-                var listItem = document.createElement('li');
+                const listItem = document.createElement('li');
                 listItem.className = 'mdl-list__item mdl-list__item--three-line';
 
-                var content = document.createElement('span');
+                const content = document.createElement('span');
                 content.className = 'mdl-list__item-primary-content';
 
-                //Recipe Title
-                var title = document.createElement('a');
+                //Recipe title
+                const title = document.createElement('a');
                 title.textContent = recipe.title;
                 //Makes title clickable to go to inividual recipe
                 title.href = "/recipe.html?recipeId=" + recipe.id;
 
                 //Recipe description
-                var description = document.createElement('span');
+                const description = document.createElement('span');
                 description.textContent = recipe.description;
                 description.className = 'mdl-list__item-text-body';
 
@@ -123,23 +125,23 @@ let buildRecipe = () => {
         method: "GET"
     }).then(
         res => res.json().then(recipe => {
-            //Recipe Title
+            //Recipe title
             document.getElementById('recipeTitle').textContent = recipe.title;
             //Description
             document.getElementById('recipeDescription').textContent = recipe.description;
-            //Cook Time
+            //Cook time
             document.getElementById('recipeTime').textContent = "Prep time: " + recipe.time;
 
-            var list = document.createElement('ul');
+            const list = document.createElement('ul');
             list.className = 'mdl-list';
 
             //Ingredients
             recipe.ingredients.forEach(ingredient => {
 
-                var listItem = document.createElement('li');
+                let listItem = document.createElement('li');
                 listItem.className = 'mdl-list__item';
 
-                var content = document.createElement('span');
+                let content = document.createElement('span');
                 content.className = 'mdl-list__item-primary-content';
 
                 content.textContent = ingredient.name + ": " + ingredient.quantity + " " + ingredient.measure;
@@ -150,4 +152,104 @@ let buildRecipe = () => {
             document.getElementById('anchor').appendChild(list);
         })
     );
+};
+
+//Lets user save a new recipe
+let saveRecipe = () => {
+
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const params = Object.fromEntries(urlSearchParams.entries());
+
+    const ingredientTable = document.getElementById("ingredient-table");
+
+    let recipe = {
+        title: document.getElementById("recipeTitle").value,
+        description: document.getElementById("recipeDescription").value,
+        userId: params.userId,
+        time: document.getElementById("recipeTime").value,
+        deleted: false,
+        ingredients: Array.from(ingredientTable.children).map(c => ({
+            name: c.children[0].children[0].children[0].value,
+            quantity: c.children[1].children[0].children[0].value,
+            measure: c.children[2].children[0].children[0].value,
+        }))
+    };
+    
+    console.log(recipe);
+
+    //Sends username and password inputs to backend
+    fetch("/api/recipes", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(
+            recipe
+        )
+    }).then(res => {
+        //If invalid username or password, display error message
+        if (res.status != 200) {
+            document.getElementById("badRecipe").style.display = "block";
+            res.json().then(json => {
+                if (json.message != null) {
+                    document.getElementById("badRecipe").innerText = json.message;
+                }
+                else {
+                    document.getElementById("badRecipe").innerText = "Incomplete/bad recipe data";
+                }
+            });
+        }
+        //If valid username and password, create new user and log them in displaying their recipes
+        else {
+            document.getElementById("badRecipe").style.display = "none";
+            res.json().then(json => {
+                window.location.assign("/recipes.html?userId=" + params.userId);
+            });
+        }
+    });
+};
+
+//Directs user back to recipes page if canceling creating a new recipe
+let cancelRecipe = () => {
+
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const params = Object.fromEntries(urlSearchParams.entries());
+
+    window.location.assign("/recipes.html?userId=" + params.userId);
+};
+
+//Adds another row to create ingredients table
+let addIngredientRow = () => {
+    const ingredientTable = document.getElementById("ingredient-table");
+    const ingredientRow = document.getElementById("ingredient-row").cloneNode(true);
+
+    //Change the labels
+    ingredientRow.children[0].children[0].children[1].innerText = "Ingredient " + (ingredientTable.childElementCount + 1);
+    ingredientRow.children[1].children[0].children[1].innerText = "Quantity " + (ingredientTable.childElementCount + 1);
+    ingredientRow.children[2].children[0].children[1].innerText = "Measure " + (ingredientTable.childElementCount + 1);
+    ingredientTable.appendChild(ingredientRow);
+    
+    //We cloned MDL textfield inputs... had to google around to figure out how to "reset" them
+    ingredientRow.children[0].children[0].classList.remove('is-upgraded');
+    ingredientRow.children[1].children[0].classList.remove('is-upgraded');
+    ingredientRow.children[2].children[0].classList.remove('is-upgraded');
+    ingredientRow.children[0].children[0].removeAttribute('data-upgraded');
+    ingredientRow.children[1].children[0].removeAttribute('data-upgraded');
+    ingredientRow.children[2].children[0].removeAttribute('data-upgraded');
+    componentHandler.upgradeDom();
+
+    //Tell MDL to reset the text on all of the inputs
+    ingredientRow.children[0].children[0].MaterialTextfield.change('');
+    ingredientRow.children[1].children[0].MaterialTextfield.change('');
+    ingredientRow.children[2].children[0].MaterialTextfield.change('');
+};
+
+//Adds another row to create ingredients table
+let removeIngredientRow = () => {
+
+    //Makes sure to not remove the first row
+    const ingredientTable = document.getElementById("ingredient-table");
+    if (ingredientTable.childElementCount > 1) {
+        ingredientTable.removeChild(ingredientTable.lastChild);
+    }
 };
